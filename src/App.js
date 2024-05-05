f24541a9e1b50179acda03cd901200ad3db0c145
Madina Walizada <madinawalizada@Madinas-MacBook-Pro.local>
2024-05-05 13:16:43 +0100

    New components has been added with the og tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from './Header'; // Import the Header component
+import Nav from './Nav'; // Import the Nav component
+import Main from './Main'; // Import the Main component
+import Footer from './Footer'; // Import the Footer component
 
 function App() {
   return (
+
     <div className="App">
-      <h1>Home Page</h1>
+      <Header /> {/* Replace <header> tag with <Header /> component */}
+      <Nav /> {/* Replace <nav> tag with <Nav /> component */}
+      <Main /> {/* Replace <main> tag with <Main /> component */}
+      <Footer /> {/* Replace <footer> tag with <Footer /> component */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* Header content goes here */}
+    </header>
+  );
+}
+
+export default Header;
